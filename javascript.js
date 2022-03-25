9f094f10ddb7c249d310543d6ffcd61304e64364
Swimming Potato <ptphong200i@gmail.com>
2022-03-25 11:22:25 +0700

    Add operate function

--- javascript.js
+++ javascript.js
@@ -13,3 +13,16 @@ function multiply(a, b) {
 function divine(a, b) {
     return a / b;
 }
+
+function operate(a, b, operation) {
+    switch (operation) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divine(a, b);
+    }
+}
