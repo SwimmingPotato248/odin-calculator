30852aa9759bf0761ae892916128cafb5c3de24c
Swimming Potato <ptphong200i@gmail.com>
2022-03-25 16:30:06 +0700

    Add function to buttons

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function add(a, b) {
-    return a + b;
+    return +a + +b;
 };
 
 function subtract(a, b) {
@@ -23,12 +23,55 @@ function operate(a, b, operation) {
         case "*":
             return multiply(a, b);
         case "/":
+            if (b === 0) return "ERROR!";
             return divine(a, b);
     }
 }
 
-let displayValue = "";
 
-function updateDisplay() {
+function updateDisplay(displayValue) {
     document.getElementById("display").textContent = displayValue;
 }
+
+let currentTotal = 0;
+let currentOperator = "";
+let currentNum = "";
+
+const numbers = document.querySelectorAll(".number");
+numbers.forEach(e => {
+    e.addEventListener("click", () => {
+        currentNum += e.textContent;
+        updateDisplay(currentNum);
+    })
+})
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach(e => {
+    e.addEventListener("click", () => {
+        equal();
+        currentOperator = e.textContent;
+    })
+})
+
+function equal() {
+    if (currentTotal === 0 || currentOperator === "") {
+        currentTotal = parseInt(currentNum);
+    } else if (currentNum !== "" && currentOperator !== "") {
+        currentTotal = operate(currentTotal, currentNum, currentOperator);
+        }
+    currentNum = "";
+    updateDisplay(currentTotal);
+}
+
+const equalSign = document.querySelector(".equal-sign");
+equalSign.addEventListener("click", equal)
+
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", () => {
+    currentTotal = 0;
+    currentNum = "";
+    currentOperator = "";
+    updateDisplay(currentTotal);
+})
+
+updateDisplay(currentTotal)
